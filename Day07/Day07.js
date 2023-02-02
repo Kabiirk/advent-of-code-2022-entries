532994717a48fb9fd3df54017ae027e9f23f3ad5
Kabiir <kabiirk@gmail.com>
2023-02-03 01:57:31 +0530

    Day07 Done

--- Day07/Day07.js
+++ Day07/Day07.js
@@ -11,68 +11,24 @@ for(var i = 0; i<input.length; i++) {
     input[i] = input[i].split(" ");
 }
 
-test_string = `$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k`;
-var test_input = test_string.split("\n")
-for(var i = 0; i<test_input.length; i++) {
-    test_input[i] = test_input[i].split(" ");
-}
-
-// Create Directory Tree
-/*
-{
-    "/":{
-        "a":{
-            "e":{
-                "i":584,
-            },
-            "f":29116,
-            "g":2557,
-            "h.list":62596,
-        }
-        "b.txt":14848514,
-        "c.dat":8504156,
-        "d":{
-            "j":4060174,
-            "d.log":8033020,
-            "d.ext":5626152,
-            "k":7214296,
-        }
-    }
-}
-*/
-function nested_assign(root_dict, path_list, value) {
-    for( var i = 0; i < path_list.length; i++ ) {
-        root_dict = root_dict[ path_list[i] ] = root_dict[ path_list[i] ] || value;
-    }
-}
 
 var active_directory = [];
 var directory_tree = {};
-/*
-for(var j = 0; j<test_input.length; j++) {
-    var std_out = test_input[j];
+
+function nested_assign( pathlist, value ){
+    var schema = directory_tree;
+    var len = pathlist.length;
+    for(var k=0; k<len-1; k++){
+        var elem = pathlist[k];
+        if(!schema[elem]){schema[elem]={}}
+        schema = schema[elem];
+    }
+    schema[pathlist[len-1]] = value;
+};
+
+// Create directory Tree
+for(var j = 0; j<input.length; j++) {
+    var std_out = input[j];
     // command
     if(std_out[0]==="$"){
         // cd
@@ -100,24 +56,64 @@ for(var j = 0; j<test_input.length; j++) {
     // dir
     else if(std_out[0]==="dir"){
         // directory_tree[active_directory[active_directory.length-1]][std_out[1]] = {};
-        nested_assign(directory_tree, active_directory, {});
+        active_directory.push(std_out[1]);
+        nested_assign(active_directory, {});
+        active_directory.pop(std_out[1]);
     }
     // files
     else{
-        var filename = std_out[1];
-        var size = std_out[0];
-        active_directory.push(filename);
-        nested_assign(directory_tree, active_directory, size);
+        // var filename = std_out[1];
+        // var size = std_out[0];
+        active_directory.push(std_out[1]);
+        nested_assign(active_directory, Number(std_out[0]));
         active_directory.pop();
     }
 }
-console.log(directory_tree);
-*/
-
-// DFS for size of each directory
+// console.log(JSON.stringify(directory_tree, undefined, 2));
 
 
+// Determine size of each directory
+const sizes = {}
+
+const dfs = (tree, path="/") => {
+    let size = 0;
+
+    Object.entries(tree).forEach(([name, content]) => {
+            size+=
+                typeof content === "object" ? dfs(content, path+">"+name) : content;
+        }
+    );
+
+    sizes[path] = size;
+    return size;
+}
+
+dfs(directory_tree)
+// console.log(sizes);
+
+// Part 1
 // Find all directories under 100000 & add up their sizes
-// console.log(test_input)
+var sum = 0;
+for (const [key, value] of Object.entries(sizes)){
+    if(value<=100000){
+        sum+=value;
+    }
+}
+console.log(sum);// 1513699
 
-var a = {}
+// Part 2
+// Total disk space available = 70000000
+// Space required for running update = 30000000
+
+var directory_sizes = [];
+// Size of Root Directory
+var total_occupied_space = Math.max( ...Object.values(sizes) );
+var remaining_space = 70000000 - total_occupied_space;
+
+for (const [key, value] of Object.entries(sizes)){
+    if(remaining_space+value > 30000000){
+        directory_sizes.push(value);
+    }
+}
+
+console.log(Math.min(...directory_sizes));// 7991939
