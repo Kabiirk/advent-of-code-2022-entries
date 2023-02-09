e041838b010d8e82cbd9fc08629dc3f1dcc86ed8
Kabiir <kabiirk@gmail.com>
2023-02-10 02:37:55 +0530

    Day08 Done

--- Day08/Day08.js
+++ Day08/Day08.js
@@ -11,21 +11,11 @@ for(var i=0; i<input.length; i++){
     input[i] = input[i].split("").map(Number);
 }
 
-var test = `30373
-25512
-65332
-33549
-35390`.split("\n")
-
-for(var i=0; i<test.length; i++){
-    test[i] = test[i].split("").map(Number);
-}
-
-function check_line(y, x, tree){
+function check_visibility(y, x, tree){
     var top = true;// x,y->top
     var bottom = true;// x,y->bottom
     var left = true;// x,y->left
-    var right = true;//// x,y->right
+    var right = true;// x,y->right
     // check top
     // console.log("TOP");
     for(var t=y-1; t>=0; t--){
@@ -63,13 +53,56 @@ function check_line(y, x, tree){
     return top || bottom || left || right;
 }
 
-function visible_tree_count(tree_grid){
-    const visible = new Set();
-    return 0;
+function get_scenic_score(y, x, tree){
+    var top = 0;// x,y->top
+    var bottom = 0;// x,y->bottom
+    var left = 0;// x,y->left
+    var right = 0;// x,y->right
+    // check top
+    for(var t=y-1; t>=0; t--){
+        if(tree[t][x]>=tree[y][x]){
+            top++;
+            break;
+        }
+        else{
+            top++;
+        }
+    }
+    // check bottom
+    for(var b=y+1; b<tree.length; b++){
+        if(tree[b][x]>=tree[y][x]){
+            bottom++;
+            break;
+        }
+        else(
+            bottom++
+        )
+    }
+    // Check left
+    for(var l=x-1; l>=0; l--){
+        if(tree[y][l]>=tree[y][x]){
+            left++;
+            break;
+        }
+        else{
+            left++;
+        }
+    }
+    // Check right
+    for(var r=x+1; r<tree.length; r++){
+        if(tree[y][r]>=tree[y][x]){
+            right++;
+            break;
+        }
+        else{
+            right++
+        }
+    }
+    // console.log("top: ",top," Left: ", left," Bottom: ", bottom," Right: ", right);
+
+    return top * left * bottom * right;
 }
 
-// console.log(test);
-// console.log(check_line(1,3,test));
 
 // Part 1
 // Account for all the corners being visible
@@ -77,12 +110,22 @@ var count = 4*(input.length-1);
 
 for(var y = 1; y<input.length-1; y++){
     for(var x = 1; x<input[y].length-1; x++){
-        if(check_line(y, x, input)){
+        if(check_visibility(y, x, input)){
             count++;
             // console.log(count, test[y][x])
         }
     }
 }
 
-console.log(count);
+console.log(count);// 1693
 
+// Part 2
+var scenic_scores = []
+
+for(var y = 1; y<input.length-1; y++){
+    for(var x = 1; x<input[y].length-1; x++){
+        scenic_scores.push(get_scenic_score(y, x, input));
+    }
+}
+
+console.log(Math.max( ...scenic_scores ))// 422059
