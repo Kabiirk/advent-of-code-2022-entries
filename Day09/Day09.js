5449602a50f50a9e065e55e876be92138028f3a8
Kabiir <kabiirk@gmail.com>
2023-02-12 01:18:33 +0530

    Day09 Done

--- Day09/Day09.js
+++ Day09/Day09.js
@@ -7,95 +7,109 @@ const input = fs
     .trim()
     .split('\n');
 
-// Test start
-var test = `R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2
-`.trim().split('\n');
-
-for(var j = 0; j<test.length; j++){
-    test[j] = test[j].split(" ");
-    test[j][1] = Number(test[j][1])
-}
-
-// Test end
-
+// Parse Input in required format
 for(var i = 0; i<input.length; i++){
     input[i] = input[i].split(" ");
     input[i][1] = Number(input[i][1])
 }
 
-function process_motion(motion, x, y){
-    // yield [x, y];
-    var direction = motion[0];
-    var length = motion[1];
-    switch (direction) {
-        case "L":
-            for(let i = 0; i<length; ++i){
-                --x;
-            }
-            return [x, y];
-            break;
-        case "R":
-            for(let i = 0; i<length; ++i){
-                ++x;
-            }
-            return [x, y];
-            break;
-        case "U":
-            for(let i = 0; i<length; ++i){
-                ++y;
-            }
-            return [x, y];
-            break;
-        case "D":
-            for(let i = 0; i<length; ++i){
-                --y;
-            }
-            return [x, y];
-            break;
-        default:
-            break;
+const move_def = {
+    R:{ x:1, y:0 },
+    L:{ x:-1, y:0 },
+    U:{ x:0, y:1 },
+    D:{ x:0, y:-1 },
+};
+
+class Point{
+    constructor(x, y){
+        this.x = 0;
+        this.y = 0;
+    }
+
+
+    move(direction){
+        const delta = move_def[direction];
+        this.x += delta.x;
+        this.y += delta.y;
+    }
+
+    follow(point){
+        const distance = Math.max(
+                                    Math.abs(this.x - point.x),
+                                    Math.abs(this.y - point.y)
+                                );
+        if(distance > 1){
+            // Move Point
+            const direction_x = point.x - this.x;
+            /*
+            0 -> do nothing
+            1 or 2 -> this.x++ or this.y++;
+            -1 or -2 -> this.x-- or this.y--;
+            */
+            this.x += Math.abs(direction_x) === 2 ? direction_x/2 : direction_x;
+            const direction_y = point.y - this.y;
+            this.y += Math.abs(direction_y) === 2 ? direction_y/2 : direction_y;
+        }
     }
 }
 
-function move_tail(tx, ty, hx, hy){
-    if(tx<hx-2 || tx>hx+2 || ty<hy-2 || ty>hy+2){
-        throw new Error("Head far away !")
-    }
-    if( !( hx-1<=tx && hx+1>=tx && hy-1<=ty && hy+1>=ty ) ){
-        tx += hx > tx ? 1 : hx < tx ? -1 : 0;
-        ty += hy > ty ? 1 : hy < ty ? -1 : 0;
-    }
-    return [tx, ty];
+function mark_visited(x, y, visited_set){
+    visited_set.add(`${x}-${y}`);
 }
 
-var tail_path = new Set();
-function process_tail_motion(motions, tail_path_set){
-    let hx = 0;
-    let hy = 0;
-    let tx = 0;
-    let ty = 0;
-    // yield [tx, ty];
+function move_rope(move_set){
+    let tail = new Point(0,0);
+    let head = new Point(0,0);
+    const visited = new Set();
+    mark_visited(0, 0, visited);
 
-    for(let k = 0; k<motions.length; k++){
-        var head = process_motion(motions[k], hx, hy);
-        hx = head[0];
-        hy = head[1];
-        var tail = move_tail(tx, ty, hx, hy);
-        tx = tail[0];
-        ty = tail[1];
-        tail_path_set.add( tx.toString()+","+ty.toString() );
-        // yield [tx, ty];
+    for (var line of move_set) {
+        var direction = line[0];
+        var total_moves = line[1];
+        for(let i = 0; i<total_moves; i++){
+            head.move(direction);
+            // console.log(head);
+            tail.follow(head);
+            mark_visited(tail.x, tail.y, visited);
+        }
     }
+
+    console.log(visited.size)
+
 }
 
-process_tail_motion(test, tail_path);
-console.log(tail_path);
+/*
+Longer rope has more knots which follow the same behaviour
+between 2 knots as part 1.
+We need to treat the rope of 10 knots as 9 smaller ropes of
+2 knots each joined together acting as 1 big rope.
+*/
+function move_longer_rope(move_set){
+    const knots = new Array(10).fill(0).map((_) => new Point(0, 0));
+    const visited = new Set();
+    mark_visited(0, 0, visited);
 
-// console.log(test)
+    for (var line of move_set) {
+        var direction = line[0];
+        var total_moves = line[1];
+        for(let i = 0; i<total_moves; i++){
+            // move head as per directions
+            knots[0].move(direction);
+            // move rest of the rope
+            for (let knot = 1; knot < knots.length; knot++) {
+                const point = knots[knot];
+                point.follow(knots[knot-1])
+            }
+            const tail = knots[knots.length - 1];
+            mark_visited(tail.x, tail.y, visited);
+        }
+    }
+
+    console.log(visited.size)
+}
+
+// Part 1
+move_rope(input);// 6503
+
+// Part 2
+move_longer_rope(input);// 2724
