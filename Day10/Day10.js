9ae29625c8a3831a12188dd1325974bde36104b8
Kabiir <kabiirk@gmail.com>
2023-02-13 02:26:00 +0530

    Day10 WIP

---
+++ Day10/Day10.js
@@ -0,0 +1,184 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = fs
+    .readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
+    .toString()
+    .trim()
+    .split('\n');
+
+
+var test = `addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`
+test = test.split("\n");
+
+// console.log(test);
+
+function add_signal_strengths(program){
+    var cycle = 0;
+    var X = 1;
+    var signal_strengths = [];
+    for(let line of program){
+        var components = line.split(" ");
+        var instruction = components[0];
+        if(instruction==="noop"){
+            cycle++;
+        }
+        else{
+            var value = components[1];
+            X+=Number(value);
+            cycle+=2;
+        }
+        if( (cycle===20) || ( (cycle-20)%40 ===0) ){
+            signal_strengths.push( cycle*X );
+            console.log(cycle*X);
+        }
+    }
+    console.log(signal_strengths);
+}
+
+add_signal_strengths(test);
