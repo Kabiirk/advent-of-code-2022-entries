8874e7effb1d6745c33c11798d241f9c357910d7
Kabiir <kabiirk@gmail.com>
2023-02-26 00:48:43 +0530

    Day12 Data Parsing Done

---
+++ Day12/Day12.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+
+const input = fs
+    .readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
+    .toString()
+    .trim()
+    
+
+function parse_input(input_graph){
+    const terrain = { dem: input_graph.split("\n").map((row) => row.split("")) };
+    terrain.nrows = terrain.dem.length;
+    terrain.ncols = terrain.dem[0].length;
+    // console.log(terrain);
+
+    // Assign start & end
+    // convert alphabets to corresponding Integers
+    for(var row = 0; row < terrain.nrows; row++){
+        for(var col = 0; col < terrain.ncols; col++){
+            if(terrain.dem[row][col] === "S"){
+                terrain.start = [row, col];
+                terrain.dem[row][col] = 0;
+            }
+            else if(terrain.dem[row][col] === "E"){
+                terrain.end = [row, col];
+                terrain.dem[row][col] = 26;
+            }
+            else{
+                terrain.dem[row][col] = terrain.dem[row][col].charCodeAt(0) - "a".charCodeAt(0);
+            }
+        }
+    }
+
+    return terrain;
+}
+
+const terrain = parse_input(input);
+console.log(terrain);
+
+function bfs(start, end){
+    const visited = new Set();
+    const queue = [[]]
+}
+
+var s = 0;
