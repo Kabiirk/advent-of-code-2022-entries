5dff3bc446c1dba5df864e35862c205072c7d337
Kabiir <kabiirk@gmail.com>
2023-03-01 23:39:23 +0530

    Day12 Part 1 WIP

--- Day12/Day12.js
+++ Day12/Day12.js
@@ -46,11 +46,45 @@ abdefghi`
 const test_terrain = parse_input(test);
 console.log(test_terrain);
 
-function bfs(start, end){
+function bfs(terrain){
     const visited = new Set();
     const queue = [];
 
-    queue.push([terrain.start]);
+    queue.push([terrain.start[0], terrain.start[1], 0]);
+    while(queue.length()){
+        const [r, c, d] = queue.shift();
+
+        // immediate neighbors
+        const next = [
+            [r+1, c],
+            [r-1, c],
+            [r, c+1],
+            [r, c-1],
+        ]
+
+        for(let i=0; i<next.length; i++){
+            const nr = next[i][0];
+            const nc = next[i][1];
+
+            if(nr<0 || nc<0 || nr>terrain.dem.length || nc>terrain.dem[0].length){
+                continue;
+            }
+            if(visited.has(`${terrain.start[0]},${terrain.start[1]}`)){
+                continue;
+            }
+            if(terrain.dem[nr][nc] - terrain.dem[r][c] > 1){
+                continue;
+            }
+            if(nr===terrain.end[0] && nc===terrain.end[1]){
+                console.log(d+1);
+            }
+
+            visited.push(`${nr},${nc}`);
+            queue.push([nr, nc, d+1]);
+        }
+    }
 }
+
+bfs(test_terrain);
 
 var s = 0;
