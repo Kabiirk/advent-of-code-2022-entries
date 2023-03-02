905be5f5833bef303d746c210f71cccfbb3d651a
Kabiir <kabiirk@gmail.com>
2023-03-02 15:10:29 +0530

    Day12 Part 1 Done

--- Day12/Day12.js
+++ Day12/Day12.js
@@ -19,14 +19,14 @@ function parse_input(input_graph){
         for(var col = 0; col < terrain.ncols; col++){
             if(terrain.dem[row][col] === "S"){
                 terrain.start = [row, col];
-                terrain.dem[row][col] = 0;
+                terrain.dem[row][col] = 1;
             }
             else if(terrain.dem[row][col] === "E"){
                 terrain.end = [row, col];
                 terrain.dem[row][col] = 26;
             }
             else{
-                terrain.dem[row][col] = terrain.dem[row][col].charCodeAt(0) - "a".charCodeAt(0);
+                terrain.dem[row][col] = terrain.dem[row][col].charCodeAt(0) - "a".charCodeAt(0) + 1;
             }
         }
     }
@@ -44,14 +44,17 @@ acctuvwj
 abdefghi`
 
 const test_terrain = parse_input(test);
-console.log(test_terrain);
+// console.log(test_terrain);
 
-function bfs(terrain){
+function traverse(terrain){
     const visited = new Set();
     const queue = [];
 
+    // initialization
     queue.push([terrain.start[0], terrain.start[1], 0]);
-    while(queue.length()){
+    visited.add(`${terrain.start[0]},${terrain.start[1]}`)
+
+    while(queue.length){
         const [r, c, d] = queue.shift();
 
         // immediate neighbors
@@ -59,32 +62,37 @@ function bfs(terrain){
             [r+1, c],
             [r-1, c],
             [r, c+1],
-            [r, c-1],
+            [r, c-1]
         ]
 
         for(let i=0; i<next.length; i++){
             const nr = next[i][0];
             const nc = next[i][1];
 
-            if(nr<0 || nc<0 || nr>terrain.dem.length || nc>terrain.dem[0].length){
+            if(nr<0 || nc<0 || nr>=terrain.dem.length || nc>=terrain.dem[0].length){
                 continue;
             }
-            if(visited.has(`${terrain.start[0]},${terrain.start[1]}`)){
+            if(visited.has(`${nr},${nc}`)){
                 continue;
             }
             if(terrain.dem[nr][nc] - terrain.dem[r][c] > 1){
                 continue;
             }
-            if(nr===terrain.end[0] && nc===terrain.end[1]){
-                console.log(d+1);
+            if(nr==terrain.end[0] && nc==terrain.end[1]){
+                return d+1;
             }
 
-            visited.push(`${nr},${nc}`);
+            visited.add(`${nr},${nc}`);
             queue.push([nr, nc, d+1]);
         }
     }
 }
 
-bfs(test_terrain);
+// Part 1
+var fewest_steps = traverse(terrain);
+console.log(fewest_steps);// 472
+
+// Part 2
+
 
 var s = 0;
