5b0bdf6af89746ba8da8fcbc21710b3319f6469b
Kabiir <kabiirk@gmail.com>
2023-03-11 01:37:00 +0530

    Day14 Done

--- Day14/Day14.js
+++ Day14/Day14.js
@@ -7,14 +7,40 @@ const input = fs
     .trim()
     .split('\n');
 
-// console.log(input)
+function parse(input_string){
+    var filled = new Set();
 
-var test = `498,4 -> 498,6 -> 496,6
-503,4 -> 502,4 -> 502,9 -> 494,9`;
+    for(const line of input_string){
+        const coords = [];
 
-var test = test.trim().split('\n')
+        for(const str_coord of line.split(' -> ')){
+            const [x, y] = str_coord.split(',').map(Number)
+            coords.push([x, y])
+        }
 
-// Part 1
+        // console.log(coords);
+        for (let i = 1; i < coords.length; i++) {
+            const [cx, cy] = coords[i];
+            // Previous coords
+            const [px, py] = coords[i-1];
+
+            if(cy !== py){
+                for (let y = Math.min(cy, py); y <= Math.max(cy, py); y++) {
+                    filled.add(`${cx},${y}`);
+                }
+            }
+            if(cx !== px){
+                for (let x = Math.min(cx, px); x <= Math.max(cx, px); x++) {
+                    filled.add(`${x},${cy}`);
+                }
+            }
+        }
+    }
+
+    return filled
+}
+
+// for Part 1
 const simulate_sand = (filled, max_y) => {
     // Sand Fall Origin
     let [x, y] = [500, 0]
@@ -47,89 +73,16 @@ const simulate_sand = (filled, max_y) => {
     return false;
 }
 
-const filled = new Set();
-
-for(const line of input){
-    const coords = [];
-
-    for(const str_coord of line.split(' -> ')){
-        const [x, y] = str_coord.split(',').map(Number)
-        coords.push([x, y])
-    }
-
-    // console.log(coords);
-    for (let i = 1; i < coords.length; i++) {
-        const [cx, cy] = coords[i];
-        // Previous coords
-        const [px, py] = coords[i-1];
-
-        if(cy !== py){
-            for (let y = Math.min(cy, py); y <= Math.max(cy, py); y++) {
-                filled.add(`${cx},${y}`);
-            }
-        }
-        if(cx !== px){
-            for (let x = Math.min(cx, px); x <= Math.max(cx, px); x++) {
-                filled.add(`${x},${cy}`);
-            }
-        }
-    }
-}
-
-// Last wall
-const max_y = Math.max(...[...filled].map(coord => Number(coord.split(',')[1])))
-
-// console.log(max_y);
-
-let ans = 0;
-while(true){
-    const res = simulate_sand(filled, max_y);
-
-    if(!res){
-        break;
-    }
-
-    ans++;
-}
-console.log(ans);// 828
-
 // Part 2
-const filled_2 = new Set();
-
-for(const line of test){
-    const coords = [];
-
-    for(const str_coord of line.split(' -> ')){
-        const [x, y] = str_coord.split(',').map(Number)
-        coords.push([x, y])
-    }
-
-    // console.log(coords);
-    for (let i = 1; i < coords.length; i++) {
-        const [cx, cy] = coords[i];
-        // Previous coords
-        const [px, py] = coords[i-1];
-
-        if(cy !== py){
-            for (let y = Math.min(cy, py); y <= Math.max(cy, py); y++) {
-                filled_2.add(`${cx},${y}`);
-            }
-        }
-        if(cx !== px){
-            for (let x = Math.min(cx, px); x <= Math.max(cx, px); x++) {
-                filled_2.add(`${x},${cy}`);
-            }
-        }
-    }
-}
-
-const max_y2 = Math.max(...[...filled_2].map(coord => Number(coord.split(',')[1])))
-
 const simulate_sand_with_floor = (filled_2, max_y2) => {
     // Sand Fall Origin
     let [x, y] = [500, 0]
 
-    while(y<=max_y2+2){
+    if( filled_2.has(`${x},${y}`) ){
+        return [x, y];
+    }
+    
+    while(y<=max_y2){
         // drop straight down
         if(!filled_2.has(`${x},${y+1}`)){
             y++;
@@ -149,28 +102,41 @@ const simulate_sand_with_floor = (filled_2, max_y2) => {
         }
 
         // Standstill
-        filled_2.add(`${x},${y}`);
-        return true;
-    }
-    console.log("YO");
-
-    // Reached till source
-    if(filled_2.has('500,0')){
-        return false;
-    }
-}
-
-let ans2 = 0;
-while(true){
-    const res2 = simulate_sand_with_floor(filled_2, max_y2);
-
-    if(!res2){
         break;
     }
 
-    ans2++;
+    // Reached till source
+    return [x, y];
 }
-console.log("P2", ans2);
 
 
-var s = 0;
+// Part 1
+var filled = parse(input);
+// Last wall
+const max_y = Math.max(...[...filled].map(coord => Number(coord.split(',')[1])))
+let sand_before_abyss = 0;
+
+while(true){
+    const res = simulate_sand(filled, max_y);
+
+    if(!res){
+        break;
+    }
+
+    sand_before_abyss++;
+}
+console.log(sand_before_abyss);// 828
+
+// Part 2
+const filled_2 = parse(input);
+let sand_till_source_filled = 0;
+
+while(true){
+    const [x, y] = simulate_sand_with_floor(filled_2, max_y);
+    filled_2.add(`${x},${y}`)
+    sand_till_source_filled++;
+    if(x===500 && y===0){
+        break;
+    }
+}
+console.log(sand_till_source_filled);// 25500
