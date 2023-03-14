4ab1387f368ffb7964f04eec63dbfa29c5f58b43
Kabiir <kabiirk@gmail.com>
2023-03-14 19:57:19 +0530

    Day15 Input parsing done

--- Day15/Day15.js
+++ Day15/Day15.js
@@ -7,4 +7,39 @@ const input = fs
     .trim()
     .split('\n');
 
-console.log(input);
+
+var test = `Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3`
+
+test = test.trim().split('\n');
+
+function parse_input(input_strings){
+    let pattern = /-?\d{1,10}/g;
+
+    const sensor_layout = []
+
+    for(var input_string of input_strings){
+        var pattern_matches = input_string.match(pattern).map(Number);
+        sensor_layout.push( {
+                                "sensor": [pattern_matches[0], pattern_matches[1]],
+                                "beacon": [pattern_matches[2], pattern_matches[3]]
+                            } )
+    }
+
+    return sensor_layout;
+}
+
+var sensor_list = parse_input(input);
+console.log(sensor_list);
