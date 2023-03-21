576bcc0784defadb4305ca9a4b299ddca7985728
Kabiir <kabiirk@gmail.com>
2023-03-22 02:10:27 +0530

    Parsing WIP

--- Day16/Day16.js
+++ Day16/Day16.js
@@ -7,4 +7,22 @@ const input = fs
     .trim()
     .split('\n');
 
-console.log(input);
+// console.log(input);
+
+const graph = {};
+const rates = {};
+
+for(var line of input){
+    // Valve AW has flow rate=0; tunnels lead to valves LG, TL
+    try {
+        var a = line.split("; tunnels lead to valves ");
+        console.log(a[1].split(", "));
+    } catch (TypeError) {
+        var a = line.split("; tunnel leads to valve ");
+        console.log(a[1].split(", "));
+    }
+    
+    // var pattern = /Valve (?<valve>-?\w+) has flow rate=(?<flow>-?\d+); tunnels lead to valves/;
+    // var pattern_matches = line.match(pattern).groups;
+    // console.log(valve, flow);
+}
