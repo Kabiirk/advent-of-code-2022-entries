cb4363128634420519927ebb8b683e895ffc8e45
Kabiir <kabiirk@gmail.com>
2023-03-23 23:02:42 +0530

    Data Parsing Done

--- Day16/Day16.js
+++ Day16/Day16.js
@@ -11,18 +11,28 @@ const input = fs
 
 const graph = {};
 const rates = {};
-
+var i = 1;
 for(var line of input){
     // Valve AW has flow rate=0; tunnels lead to valves LG, TL
+    var edges = []
     try {
         var a = line.split("; tunnels lead to valves ");
-        console.log(a[1].split(", "));
+        // console.log(i, a[1].split(", "));
+        edges = a[1].split(", ");
     } catch (TypeError) {
         var a = line.split("; tunnel leads to valve ");
-        console.log(a[1].split(", "));
+        // console.log(i, a[1].split(", "));
+        edges = a[1].split(", ");
     }
-    
-    // var pattern = /Valve (?<valve>-?\w+) has flow rate=(?<flow>-?\d+); tunnels lead to valves/;
-    // var pattern_matches = line.match(pattern).groups;
-    // console.log(valve, flow);
+    // console.log(edges);
+    i++;
+    var pattern = /Valve (?<valve>-?\w+) has flow rate=(?<flow>-?\d+)/;
+    var pattern_matches = a[0].match(pattern).groups;
+    // console.log(pattern_matches.valve, Number(pattern_matches.flow));
+    graph[pattern_matches.valve] = edges;
+    rates[pattern_matches.valve] = Number(pattern_matches.flow);
+    // console.log(a[0]);
 }
+
+console.log(graph);
+console.log(rates);
