956daaf07bf9faaa21ae5a046feb47dae6a74ac0
Kabiir <kabiirk@gmail.com>
2023-03-25 02:15:14 +0530

    Day16 BFS WIP

--- Day16/Day16.js
+++ Day16/Day16.js
@@ -23,6 +23,7 @@ test = test.split("\n");
 function parse_input(text_input){
     const graph = {};
     const rates = {};
+    const distance = {};
     // var i = 1;
     for(var line of text_input){
         // Valve AW has flow rate=0; tunnels lead to valves LG, TL
@@ -43,23 +44,69 @@ function parse_input(text_input){
         // console.log(pattern_matches.valve, Number(pattern_matches.flow));
         graph[pattern_matches.valve] = edges;
         rates[pattern_matches.valve] = Number(pattern_matches.flow);
+        distance[pattern_matches.valve] = 0;
         // console.log(a[0]);
     }
 
-    return [graph, rates];
+    return [graph, rates, distance];
 }
 
-[graph, rates] = parse_input(test);
+[graph, rates, distance] = parse_input(test);
 console.log(graph);
-console.log(rates);
+// console.log(rates);
+/*
+dist = {
+    AA : {DD: 1, II:2, BBL3};
+    ...
+}
+*/
 
+function compress_graph(graph, rates){
+    var g = graph;
+    for(const valve in g){
+        g[valve].dist = {}
+        if(valve !== "AA" && rates[valve]===0){ continue; }
+        const visited = new Set();
+        const q = [[0, valve]];
+
+        // bfs
+        while(q.length > 0){
+            const [dist, v] = q.shift();
+            if(visited.has(v)){ continue; }
+            visited.add(v);
+            if(rates[v]>0){
+                g[valve].dists[v] = dist;
+            }
+            // g[v].edges.forEach(neighbor => {
+            //     console.log(neighbor);
+            //     // q.push([dist+1, neighbor]);
+            // });
+            for(var neighbor of g[v]){
+                q.push([dist+1, neighbor]);
+            }
+        }
+        delete g[valve].dists[valve];
+    }
+
+    for(var valve in g){
+        delete g[valve]
+        if(valve !== "AA" && rates[valve]===0){
+            delete g[valve];
+        }
+    }
+
+    console.log(g);
+    // return g;
+}
+
+compress_graph(graph, rates);
 
 // function bfs(graph, rates){
 //     const time = 30;
-    
+
 //     // BFS
 //     const queue = [];
-    
+
 //     var root = "AA";
 //     var flow = 0;
 //     var open_valves = [];
