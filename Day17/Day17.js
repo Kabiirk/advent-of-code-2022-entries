da50ccc9a48987a804bac00b4f750fc7e605b901
Kabiir <kabiirk@gmail.com>
2023-04-04 23:57:01 +0530

    Day17 custom Rock class WIP

--- Day17/Day17.js
+++ Day17/Day17.js
@@ -8,3 +8,49 @@ const input = fs
     .split('\n');
 
 console.log(input);
+
+
+/*
+The 5 Rocks
+
+####
+
+------
+
+.#.
+###
+.#.
+
+------
+
+..#
+..#
+###
+
+------
+
+#
+#
+#
+#
+
+------
+
+##
+##
+ */
+
+// The Rock
+class Dwayne{
+    constructor(height, width, shape){
+        this.height = height;
+        this.width = width;
+        this.shape = shape;
+    }
+    toString(){
+        return this.shape
+    }
+}
+
+var test = new Dwayne(1, 4, '####');
+console.log(test);
