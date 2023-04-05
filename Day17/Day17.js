f2eaad3296f69f476993411a7c7905575abca597
Kabiir <kabiirk@gmail.com>
2023-04-06 00:17:34 +0530

    Day17 Alt approach WIP

--- Day17/Day17.js
+++ Day17/Day17.js
@@ -1,128 +1,119 @@
-const fs = require('fs');
+const fs = require("fs")
+const performance = require("perf_hooks").performance
+const eol = require("os").EOL
 const path = require('path');
+ 
+let startTime = performance.now()
+let part1 = (part2 = 0)
+let input = fs
+    .readFileSync(path.join(__dirname, 'input.txt'), "utf8")
+    .split(eol)
+    .join("")
+    .split("")
 
-const input = fs
-    .readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
-    .toString()
-    .trim()
-    .split('\n');
-
-console.log(input);
-
-
-/*
-The 5 Rocks
-
-####
-
-------
-
-.#.
-###
-.#.
-
-------
-
-..#
-..#
-###
-
-------
-
-#
-#
-#
-#
-
-------
-
-##
-##
- */
-
-// The Rock
-class Dwayne{
-    constructor(height, width, shape){
-        this.height = height;
-        this.width = width;
-        this.shape = shape;
-    }
-    toString(){
-        return this.shape
-    }
+console.log(input)
+let chamber = [["#", "#", "#", "#", "#", "#", "#", "#", "#"]]
+let segment = ["#", ".", ".", ".", ".", ".", ".", ".", "#"]
+ 
+let bloks = [
+    [["#", "#", "#", "#"]],
+    [
+        [".", "#", "."],
+        ["#", "#", "#"],
+        [".", "#", "."]
+    ],
+    [
+        ["#", "#", "#"],
+        [".", ".", "#"],
+        [".", ".", "#"]
+    ],
+    [["#"], ["#"], ["#"], ["#"]],
+    [
+        ["#", "#"],
+        ["#", "#"]
+    ]
+]
+let steps = 1000000000000
+let blockIndex = 0
+let numBlocks = bloks.length
+let numjets = input.length
+let topIndex = 1
+let jetIndex = 0
+ 
+function colides(block, blockX, blockY) {
+    return block.some((r, y) => {
+        return r.some((state, x) => {
+            return state == "#" && chamber[blockY + y][blockX + x] == "#"
+        })
+    })
 }
-
-var test = new Dwayne(1, 4, '####');
-console.log(test);
-
-const settledRock = new Set();
-
-const checkCollision = (rock) => {
-  for (let piece of rock) {
-    if (settledRock.has(`${piece.x},${piece.y}`)) return "ROCK";
-    if (piece.y <= 0) return "FLOOR";
-    if (piece.x <= 0 || piece.x >= 8) return "WALL";
-  }
-  return false;
-};
-
-const updatePosition = (rock, tallestPoint) => {
-  rock.forEach((piece) => {
-    piece.y += tallestPoint + 4;
-    piece.x += 3;
-  });
-  return rock;
-};
-
-const findTallestPoint = (rock, tallestPoint) => {
-  for (let piece of rock) {
-    tallestPoint = Math.max(tallestPoint, piece.y);
-  }
-  return tallestPoint;
-};
-
-let remainingPattern = [...pattern];
-let tallestPoint = 0;
-for (let i = 0; i < 2022; i++) {
-  let rocksCopy = JSON.parse(JSON.stringify(rocks));
-
-  let rock = rocksCopy[i % rocks.length];
-  rock = updatePosition(rock, tallestPoint);
-
-  let isFallNextMove = false;
-  while (true) {
-    if (isFallNextMove) {
-      rock.forEach((piece) => {
-        piece.y -= 1;
-      });
-      let checkCollisionResult = checkCollision(rock);
-      if (checkCollisionResult === "ROCK" || checkCollisionResult === "FLOOR") {
-        // Revert the fall and add the rock to the settledRock set
-        rock.forEach((piece) => {
-          piece.y += 1;
-          settledRock.add(`${piece.x},${piece.y}`);
-        });
-        tallestPoint = findTallestPoint(rock, tallestPoint);
-        break;
-      }
-    } else {
-      let xMove = remainingPattern.shift() === ">" ? 1 : -1;
-      if (remainingPattern.length === 0) remainingPattern = [...pattern];
-      rock.forEach((piece) => {
-        piece.x += xMove;
-      });
-      let checkCollisionResult = checkCollision(rock);
-      if (checkCollisionResult === "ROCK" || checkCollisionResult === "WALL") {
-        // Revert the move
-        rock.forEach((piece) => {
-          piece.x -= xMove;
-        });
-      }
+ 
+function checkRepeat() {
+    let l = topIndex - 1
+    let max = ~~(chamber.length / 2) - 5
+    let len = max
+    for (len; len > input.length / 5; len--) {
+        let same = true
+        for (let i = 0; i < len; i++) {
+            if (!chamber[l - i].every((el, ix) => el === chamber[l - (i + len)][ix])) {
+                same = false
+                break
+            }
+        }
+        if (same) {
+            return len
+        }
     }
-    isFallNextMove = !isFallNextMove;
-  }
+    return -1
 }
-
-console.log(tallestPoint);
-
-console.timeEnd("ExecutionTime");
+let repeatFound = false
+let repeatLength = 0
+let repeatNext = 0
+let repeatStep = 0
+let mult = 0
+while (steps--) {
+    let block = bloks[blockIndex]
+    let blockHeight = block.length
+    let blockY = topIndex + 3
+    let blockX = 3
+    while (blockY + blockHeight > chamber.length) {
+        chamber.push(segment.slice())
+    }
+ 
+    while (true) {
+        let jet = input[jetIndex++ % numjets] == "<" ? -1 : +1
+        if (!colides(block, blockX + jet, blockY)) blockX += jet
+        if (!colides(block, blockX, blockY - 1)) {
+            blockY--
+        } else {
+            block.forEach((r, y) => {
+                r.forEach((state, x) => {
+                    if (state == "#") chamber[blockY + y][blockX + x] = state
+                })
+            })
+            topIndex = Math.max(blockY + blockHeight, topIndex)
+            break
+        }
+    }
+    if (mult === 0) {
+        if (!repeatFound) {
+            let repeat = checkRepeat()
+            if (repeat != -1) {
+                repeatFound = true
+                repeatNext = topIndex + repeat
+                repeatLength = repeat
+                repeatStep = steps
+            }
+        } else if (repeatNext == topIndex) {
+            repeatStep = repeatStep - steps
+            mult = Math.floor(steps / repeatStep)
+            let rest = steps % repeatStep
+            steps = rest
+        }
+    }
+    if (1000000000000 - steps == 2022) part1 = topIndex - 1
+    if (++blockIndex == numBlocks) blockIndex = 0
+}
+part2 = topIndex - 1 + mult * repeatLength
+let time = performance.now() - startTime
+console.log(`Part 1: ${part1}\nPart 2: ${part2}\nTimer: ${time} ms`)
