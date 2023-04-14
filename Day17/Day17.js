43337e1de400c92255086ed994d4ce4553df7258
Kabiir <kabiirk@gmail.com>
2023-04-15 02:18:50 +0530

    Day17 Part 1 WIP

--- Day17/Day17.js
+++ Day17/Day17.js
@@ -7,7 +7,7 @@ const input = fs
     .trim()
     .split('')
 
-console.log(input);
+// console.log(input);
 
 
 /*
@@ -53,7 +53,7 @@ class Dwayne{
 }
 
 var test = new Dwayne(1, 4, '####');
-console.log(test);
+// console.log(test);
 
 // Alt approach
 
@@ -83,22 +83,98 @@ var dwayne = [
     ]
 ]
 
-function collision(block, blockx, blocky){
-    return block.some( (r, y) => {
-            return r.some( (state, x) => {
-                    return state == "#" && chamber[blocky+y][blockx+x] == "#"
-                }
-            )
-        }
-    )
-}
-
 let chamber = [["#", "#", "#", "#", "#", "#", "#", "#", "#"]]
 let segment = ["#", ".", ".", ".", ".", ".", ".", ".", "#"]
 let steps = 1000000000000;
-let numBlocks = bloks.length
+let numBlocks = dwayne.length
 let numjets = input.length
 let topIndex = 1
 let jetIndex = 0
+let blockIndex = 0
+let repeatFound = false
+let repeatLength = 0
+let repeatNext = 0
+let repeatStep = 0
+let mult = 0
 
-console.log(dwayne);
+function collision(block, blockX, blockY) {
+    return block.some((r, y) => {
+        return r.some((state, x) => {
+            return state == "#" && chamber[blockY + y][blockX + x] == "#"
+        })
+    })
+}
+
+function checkRepeat() {
+    let l = topIndex - 1
+    let max = ~~(chamber.length / 2) - 5
+    let len = max
+    for (len; len > input.length / 5; len--) {
+        let same = true
+        for (let i = 0; i < len; i++) {
+            if (!chamber[l - i].every((el, ix) => el === chamber[l - (i + len)][ix])) {
+                same = false
+                break
+            }
+        }
+        if (same) {
+            return len
+        }
+    }
+    return -1
+}
+
+var part_1 = 0;
+
+while(steps--){
+    let block = dwayne[blockIndex]
+    let blockHeight = block.length
+    let blockY = topIndex+3;
+    let blockX = 3;
+    while(blockY + blockHeight > chamber.length){
+        chamber.push(segment.slice())
+    }
+
+    while(true){
+        let jet = input[jetIndex++ % numjets] == "<" ? -1:1;
+        if(!collision(blockX, blockX+jet, blockY)){
+            blockX+=jet;
+        }
+        if(!collision(blockX, blockX, blockY-1)){
+            blockY--;
+        }
+        else{
+            block.forEach((r, y) => {
+                r.forEach((state, x) => {
+                    if (state == "#") chamber[blockY + y][blockX + x] = state
+                })
+            })
+            topIndex = Math.max(blockY + blockHeight, topIndex)
+            break
+        }
+    }
+
+    if (mult === 0) {
+        if (!repeatFound) {
+            let repeat = checkRepeat()
+            if (repeat != -1) {
+                repeatFound = true
+                repeatNext = topIndex + repeat
+                repeatLength = repeat
+                repeatStep = steps
+            }
+        } else if (repeatNext == topIndex) {
+            repeatStep = repeatStep - steps
+            mult = Math.floor(steps / repeatStep)
+            let rest = steps % repeatStep
+            steps = rest
+        }
+    }
+    if (1000000000000 - steps == 2022) part1 = topIndex - 1
+    if (++blockIndex == numBlocks) blockIndex = 0
+}
+
+console.log(part_1);
+
+
+// console.log(dwayne);
