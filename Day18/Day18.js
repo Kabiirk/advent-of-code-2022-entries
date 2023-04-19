ccb940848da4e2eb99dedf577ae5a959198f77f7
Kabiir <kabiirk@gmail.com>
2023-04-19 23:46:52 +0530

    Day18 Parsing Done, solving WIP

--- Day18/Day18.js
+++ Day18/Day18.js
@@ -5,6 +5,18 @@ const input = fs
     .readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
     .toString()
     .trim()
-    .split('\n');
+    .split('\n').map(
+        (x) => x.split(',').map(Number)
+    );
 
-console.log(input)
+// console.log(input)
+
+function is_adjacent(cube_1, cube_2){
+    var x_abs = Math.abs(cube_1[0]-cube_2[0])===1;
+    var y_abs = Math.abs(cube_1[1]-cube_2[1])===1;
+    var z_abs = Math.abs(cube_1[2]-cube_2[2])===1;
+
+    return x_abs || y_abs || z_abs
+}
+
+console.log(is_adjacent([1,1,1], [2,1,1]));
