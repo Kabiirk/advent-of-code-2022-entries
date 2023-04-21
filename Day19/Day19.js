90267f09a7d4fd2200720ff1d107f1a5b7554f54
Kabiir <kabiirk@gmail.com>
2023-04-21 23:52:32 +0530

    Day19 Begin + Parsing WIP

---
+++ Day19/Day19.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+var pattern = /Blueprint (?<blueprint_no>-?\d+): Each (?<robot_1>-?\w+) robot costs (?<robot_1_ore_cost>-?\d+) ore. Each (?<robot_2>-?\w+) robot costs (?<robot_2_ore_cost>-?\d+) ore. Each (?<robot_3>-?\w+) robot costs (?<robot_3_ore_cost>-?\d+) ore and (?<robot_3_clay_cost>-?\d+) clay. Each (?<robot_4>-?\w+) robot costs (?<robot_4_ore_cost>-?\d+) ore and (?<robot_4_obsidian_cost>-?\d+) obsidian./;
+const input = fs
+    .readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
+    .toString()
+    .trim()
+    .split('\n').map(
+        (x) => JSON.parse(x.match(pattern).groups)
+    );
+
+console.log(input[0]);
