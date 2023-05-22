c98d140c2a562e595c9898e56caed700e5158bac
Kabiir <kabiirk@gmail.com>
2023-05-23 01:33:39 +0530

    Day19 WIP

--- Day19/Day19.js
+++ Day19/Day19.js
@@ -65,6 +65,12 @@ class Factory {
       this.clay >= this.blueprint.obsidianCostClay
     );
   }
+  canBuildObsidianrobot() {
+    return (
+      this.ore >= this.blueprint.obsidianCostOre &&
+      this.clay >= this.blueprint.obsidianCostClay
+    );
+  }
 
   canBuildGeoderobot() {
     return (
